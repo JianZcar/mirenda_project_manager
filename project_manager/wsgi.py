fb0f318a354f491c64fe8994e0b012065c5eb314
JianZcar <jianstbn@gmail.com>
2024-01-04 16:08:06 +0800

    revert

--- project_manager/wsgi.py
+++ project_manager/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_manager.settings")
 
 application = get_wsgi_application()
 app = application
